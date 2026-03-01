37a5964693f97d625981c4b143e7e9aab44677dc
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-01 23:37:50 +0530

    Delete backend directory

--- backend/models/Blog.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const schema=new mongoose.Schema({
-  title:String,
-  content:String,
-  image:String,
-  author:String
-},{timestamps:true});
-export default mongoose.model("Blog",schema);
--- backend/models/Order.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const schema=new mongoose.Schema({
-  user:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
-  items:Array,
-  totalPrice:Number,
-  status:{type:String,default:"Pending"}
-},{timestamps:true});
-export default mongoose.model("Order",schema);
