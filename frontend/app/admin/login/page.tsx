8024413a9ee0db16baeb8712f67ca358fb9308d8
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-01 17:14:42 +0530

    Update page.tsx

--- frontend/app/admin/login/page.tsx
+++ frontend/app/admin/login/page.tsx
@@ -1,49 +1,45 @@
 "use client";
+
 import { useState } from "react";
-import { apiRequest } from "@/lib/api";
-import { saveToken } from "@/lib/auth";
 import { useRouter } from "next/navigation";
 
-export default function AdminLogin() {
+export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  async function handleLogin() {
-    const res = await apiRequest("/api/auth/login", "POST", {
-      email,
-      password,
-    });
+  async function handleLogin(e: any) {
+    e.preventDefault();
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+      }
+    );
 
-    if (res.role !== "admin") {
-      alert("Not authorized");
-      return;
-    }
-
-    saveToken(res.token);
+    const data = await res.json();
+    localStorage.setItem("token", data.token);
     router.push("/admin/dashboard");
   }
 
   return (
-    <div className="flex flex-col items-center mt-20">
-      <h1 className="text-2xl mb-4">Admin Login</h1>
+    <form onSubmit={handleLogin} className="max-w-md mx-auto space-y-4">
       <input
-        className="border p-2 mb-2"
+        className="w-full border p-2"
         placeholder="Email"
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
+        className="w-full border p-2"
         type="password"
-        className="border p-2 mb-2"
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button
-        onClick={handleLogin}
-        className="bg-black text-white px-4 py-2"
-      >
+      <button className="bg-black text-white px-4 py-2 w-full">
         Login
       </button>
-    </div>
+    </form>
   );
 }
