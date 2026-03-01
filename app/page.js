e87a6f34bcb3519eb01989523641d35737d3800f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-02 00:00:59 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>ShopNative is Live 🚀</h1>
+      <p>Your e-commerce store setup has started successfully.</p>
+    </main>
+  );
+}
