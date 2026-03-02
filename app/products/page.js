cfe2a4e98676d69fbb0ed58d4c1c8958592dfe37
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-02 23:25:21 +0530

    Create page.js

---
+++ app/products/page.js
@@ -0,0 +1,13 @@
+export default function Products() {
+  return (
+    <div style={{ padding: "100px 60px", fontFamily: "Georgia, serif" }}>
+      <h1 style={{ fontSize: "48px", marginBottom: "30px" }}>
+        Our Products
+      </h1>
+
+      <p style={{ fontSize: "20px", color: "#5c4634" }}>
+        Product listing will appear here.
+      </p>
+    </div>
+  );
+}
