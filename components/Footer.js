b4a543485024d05b603d07ccd61e9ea45a9329b4
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-12 02:48:41 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+
+return(
+
+<footer
+style={{
+background:"#3a2a1c",
+color:"#fff",
+padding:"40px",
+marginTop:"80px",
+textAlign:"center"
+}}
+>
+
+<p>© {new Date().getFullYear()} Native</p>
+
+<p>Eat Healthy, Stay Healthy</p>
+
+</footer>
+
+)
+
+}
