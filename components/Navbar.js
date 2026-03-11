e97d32e1b8541c29abb00735db88958315184a8b
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-12 02:35:38 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,3 @@
-
 "use client"
 
 import Link from "next/link"
@@ -8,49 +7,65 @@ export default function Navbar() {
 
   const { cart } = useCart()
 
-  const cartCount = cart.reduce(
-    (total, item) => total + item.quantity,
-    0
-  )
-
   return (
 
-    <nav
+    <header
       style={{
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        padding: "20px 80px",
+        padding: "15px 40px",
         borderBottom: "1px solid #eee",
-        background: "#fff"
+        background: "#fff",
+        position: "sticky",
+        top: 0,
+        zIndex: 1000
       }}
     >
 
-      <Link
-        href="/"
-        style={{
-          fontSize: "24px",
-          fontWeight: "bold",
-          textDecoration: "none",
-          color: "#3a2a1c"
-        }}
-      >
-        Native
+      {/* LOGO */}
+
+      <Link href="/" style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <img
+          src="/logo.png"
+          alt="Native"
+          style={{ height: "50px", width: "50px", objectFit: "contain" }}
+        />
+
+        <span
+          style={{
+            fontSize: "22px",
+            fontWeight: "600",
+            color: "#3a2a1c"
+          }}
+        >
+          Native
+        </span>
       </Link>
 
 
-      <div style={{ display: "flex", gap: "30px" }}>
+      {/* NAVIGATION */}
 
-        <Link href="/products">Products</Link>
+      <nav
+        style={{
+          display: "flex",
+          gap: "30px",
+          alignItems: "center"
+        }}
+      >
+
+        <Link href="/">Home</Link>
 
         <Link href="/cart">
-          Cart ({cartCount})
+          Cart ({cart.length})
         </Link>
 
-      </div>
+        <Link href="/login">
+          Login
+        </Link>
 
-    </nav>
+      </nav>
 
+    </header>
   )
-
 }
