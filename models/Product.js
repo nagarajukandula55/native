934f63abb523fc8f29aff5e311922d845023570a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-12 00:06:59 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, default: "" },
-  price: { type: Number, required: true },
-  image: { type: String, default: "" },   // URL from Cloudinary
-  alt: { type: String, default: "" },     // optional alt text for images
-  category: { type: String, default: "General" }, // future category
-  stock: { type: Number, default: 100 },  // stock count
-  featured: { type: Boolean, default: false }, // for homepage highlight
-  slug: { type: String, unique: true },    // SEO friendly
-}, { timestamps: true });
+const ProductSchema = new mongoose.Schema(
+  {
+    name: String,
+    slug: String,
+    price: Number,
+    description: String,
+    category: String,
+    stock: Number,
+    featured: Boolean,
+    image: String
+  },
+  { timestamps: true }
+)
 
-export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema)
