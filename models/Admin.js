a0228bb16e81e637c553ac1a2d0815db654f5171
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-14 04:58:55 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,9 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
-const AdminSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, default: "admin" },
-});
+const adminSchema = new mongoose.Schema({
 
-export default mongoose.models.Admin || mongoose.model("Admin", AdminSchema);
+  name:String,
+
+  email:{
+    type:String,
+    unique:true
+  },
+
+  password:String,
+
+  role:{
+    type:String,
+    default:"admin"
+  },
+
+  createdAt:{
+    type:Date,
+    default:Date.now
+  }
+
+})
+
+export default mongoose.models.Admin ||
+mongoose.model("Admin", adminSchema)
