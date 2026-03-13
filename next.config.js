08e326f0d42368a84381c5cf4e56647d54840e3a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-14 03:42:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
-  images: {
-    domains: ["res.cloudinary.com"]
-  }
-
+  output: "standalone"
 }
 
 module.exports = nextConfig
