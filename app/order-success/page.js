a09fde3f09b314194d16ac42d9efa09812475e6f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-14 04:39:18 +0530

    Update page.js

--- app/order-success/page.js
+++ app/order-success/page.js
@@ -1,8 +1,44 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import Link from "next/link"
+
 export default function OrderSuccess(){
-return(
-<div style={{textAlign:"center",marginTop:"80px"}}>
-<h1>✅ Order Placed Successfully</h1>
-<p>We will contact you soon</p>
-</div>
-)
+
+  const params = useSearchParams()
+  const orderId = params.get("orderId")
+
+  return(
+
+    <div style={{padding:"60px", textAlign:"center"}}>
+
+      <h1>🎉 Order Placed Successfully</h1>
+
+      <p style={{fontSize:"18px", marginTop:"20px"}}>
+        Your Order ID is
+      </p>
+
+      <h2 style={{color:"green"}}>{orderId}</h2>
+
+      <p style={{marginTop:"20px"}}>
+        Thank you for shopping with Native ❤️
+      </p>
+
+      <Link href="/products">
+        <button style={{
+          marginTop:"30px",
+          padding:"12px 25px",
+          background:"#2c7a4b",
+          color:"#fff",
+          border:"none",
+          cursor:"pointer"
+        }}>
+          Continue Shopping
+        </button>
+      </Link>
+
+    </div>
+
+  )
+
 }
