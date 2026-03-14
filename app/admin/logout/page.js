2fb0356e22586bef86caa205ffdf8a142d2e8015
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-14 05:30:26 +0530

    Update page.js

--- app/admin/logout/page.js
+++ app/admin/logout/page.js
@@ -1,10 +1,9 @@
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
-export default function Logout(){
+export default function Logout() {
 
   cookies().delete("adminToken")
 
   redirect("/admin/login")
-
 }
