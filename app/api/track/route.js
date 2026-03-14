21aeea06b5699845e90403d0b75173673a1f4928
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-14 05:41:05 +0530

    Create route.js

---
+++ app/api/track/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server"
+import connectDB from "@/lib/connectDB"
+import Order from "@/models/Order"
+
+export async function POST(req){
+
+  try{
+
+    await connectDB()
+
+    const body = await req.json()
+
+    const order = await Order.findOne({
+      orderId: body.orderId,
+      phone: body.phone
+    })
+
+    if(!order){
+      return NextResponse.json({
+        success:false,
+        message:"Order not found"
+      })
+    }
+
+    return NextResponse.json({
+      success:true,
+      order
+    })
+
+  }catch(err){
+
+    return NextResponse.json({
+      success:false,
+      message:"Tracking failed"
+    })
+
+  }
+
+}
