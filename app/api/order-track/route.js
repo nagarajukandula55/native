b0da5e5681bb09c7b8a5a42e8f71600f1ee789dc
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-16 02:06:26 +0530

    Create route.js

---
+++ app/api/order-track/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server"
+import connectDB from "@/lib/mongodb"
+import Order from "@/models/Order"
+
+export async function GET(req){
+
+  try{
+
+    await connectDB()
+
+    const { searchParams } = new URL(req.url)
+    const id = searchParams.get("id")
+
+    if(!id){
+      return NextResponse.json({ success:false })
+    }
+
+    const order = await Order.findOne({
+      orderId: id.trim()
+    })
+
+    if(!order){
+      return NextResponse.json({ success:false })
+    }
+
+    return NextResponse.json({
+      success:true,
+      order
+    })
+
+  }catch(e){
+
+    console.log(e)
+
+    return NextResponse.json({ success:false })
+  }
+
+}
