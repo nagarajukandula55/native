18f98df76bf779d5ed6308792fc3153862aeea5f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-16 02:01:42 +0530

    Update page.js

--- app/track/page.js
+++ app/track/page.js
@@ -1,91 +1,25 @@
-"use client"
+import { NextResponse } from "next/server"
+import connectDB from "@/lib/mongodb"
+import Order from "@/models/Order"
 
-import { useState } from "react"
+export async function GET(req){
 
-export default function TrackPage(){
+  await connectDB()
 
-  const [orderId,setOrderId] = useState("")
-  const [order,setOrder] = useState(null)
-  const [loading,setLoading] = useState(false)
-  const [error,setError] = useState("")
+  const { searchParams } = new URL(req.url)
+  const id = searchParams.get("id")
 
-  async function trackOrder(){
+  const allOrders = await Order.find().select("orderId")
 
-    if(!orderId){
-      alert("Enter Order ID")
-      return
-    }
+  console.log("TRACK SEARCH:", id)
+  console.log("ALL ORDERS:", allOrders)
 
-    setLoading(true)
-    setError("")
-    setOrder(null)
+  const order = await Order.findOne({ orderId:id })
 
-    try{
-
-      const res = await fetch(
-        "/api/track?id=" + orderId.trim()
-      )
-
-      const data = await res.json()
-
-      console.log("TRACK RESPONSE:",data)
-
-      if(data.success){
-        setOrder(data.order)
-      }else{
-        setError("Order not found")
-      }
-
-    }catch(e){
-      setError("Server error")
-    }
-
-    setLoading(false)
-  }
-
-  return(
-
-    <div style={{padding:"40px"}}>
-
-      <h1>Track Your Order</h1>
-
-      <input
-        value={orderId}
-        onChange={e=>setOrderId(e.target.value)}
-        placeholder="Enter Order ID"
-        style={{
-          padding:"10px",
-          width:"300px",
-          marginRight:"10px"
-        }}
-      />
-
-      <button onClick={trackOrder}>
-        Track
-      </button>
-
-      {loading && <p>Checking...</p>}
-
-      {error && <p style={{color:"red"}}>{error}</p>}
-
-      {order && (
-
-        <div style={{
-          marginTop:"30px",
-          padding:"20px",
-          background:"#fff",
-          borderRadius:"10px"
-        }}>
-
-          <h3>Order: {order.orderId}</h3>
-          <p>Status: {order.status}</p>
-          <p>Total: ₹ {order.totalAmount}</p>
-
-        </div>
-
-      )}
-
-    </div>
-  )
-
+  return NextResponse.json({
+    searching:id,
+    totalOrders: allOrders.length,
+    dbOrders: allOrders,
+    found: order ? true : false
+  })
 }
