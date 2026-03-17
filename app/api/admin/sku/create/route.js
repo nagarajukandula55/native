1c0869a564992ca805036352579b16f94dc64246
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 13:27:05 +0530

    Update route.js

--- app/api/admin/sku/create/route.js
+++ app/api/admin/sku/create/route.js
@@ -5,15 +5,32 @@ import SKU from "@/models/SKU"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
+  try {
+    await connectDB()
 
-  await connectDB()
+    const body = await req.json()
 
-  const body = await req.json()
+    if (!body.productId)
+      return NextResponse.json(
+        { success: false, message: "Product required" },
+        { status: 400 }
+      )
 
-  const sku = await SKU.create(body)
+    const sku = await SKU.create(body)
 
-  return NextResponse.json({
-    success: true,
-    sku
-  })
+    return NextResponse.json({
+      success: true,
+      data: sku
+    })
+  } catch (err) {
+    console.log("SKU CREATE ERROR:", err)
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: err.message
+      },
+      { status: 500 }
+    )
+  }
 }
