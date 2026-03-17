bee7aa59eaf9e15bb5a5e45c0eb7f119a3a8f79f
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 15:58:03 +0530

    Update route.js

--- app/api/admin/sku/create/route.js
+++ app/api/admin/sku/create/route.js
@@ -1,36 +1,39 @@
 export const dynamic = "force-dynamic"
-
+import { NextResponse } from "next/server"
 import connectDB from "@/lib/db"
 import SKU from "@/models/SKU"
-import { NextResponse } from "next/server"
 
-export async function POST(req) {
-  try {
+export async function POST(req){
+
+  try{
+
     await connectDB()
 
     const body = await req.json()
 
-    if (!body.productId)
-      return NextResponse.json(
-        { success: false, message: "Product required" },
-        { status: 400 }
-      )
+    if(!body.productId)
+      return NextResponse.json({success:false,message:"Product missing"})
 
-    const sku = await SKU.create(body)
+    const sku = await SKU.create({
+      productId: body.productId,
+      skuCode: body.skuCode,
+      partCode: body.partCode,
+      price: body.price
+    })
 
     return NextResponse.json({
-      success: true,
-      data: sku
+      success:true,
+      sku
     })
-  } catch (err) {
-    console.log("SKU CREATE ERROR:", err)
 
-    return NextResponse.json(
-      {
-        success: false,
-        message: err.message
-      },
-      { status: 500 }
-    )
+  }catch(err){
+
+    console.log("SKU CREATE ERROR",err)
+
+    return NextResponse.json({
+      success:false,
+      message:"SKU create failed"
+    })
   }
+
 }
