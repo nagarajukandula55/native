1582d66f5394f2f9e93402fb6ca1df04c4637237
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 18:09:47 +0530

    Update route.js

--- app/api/admin/sku/list/route.js
+++ app/api/admin/sku/list/route.js
@@ -1,15 +1,13 @@
 export const dynamic = "force-dynamic"
 
 import { NextResponse } from "next/server"
-import connectDB from "@/lib/db"
-import Sku from "@/models/Sku"
+import connectDB from "../../../../lib/db"
+import Sku from "../../../../models/Sku"
 
 export async function GET() {
   try {
     await connectDB()
-    const skus = await Sku.find({})
-      .populate("product")
-      .populate("warehouse")
+    const skus = await Sku.find({}).populate("product").populate("warehouse")
     return NextResponse.json({ success: true, skus })
   } catch (err) {
     return NextResponse.json({ success: false, error: err.message })
