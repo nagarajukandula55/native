4098e935ad67eeb914947cd5974f2ebbbf90325e
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 16:32:28 +0530

    Update route.js

--- app/api/admin/warehouse/list/route.js
+++ app/api/admin/warehouse/list/route.js
@@ -1,13 +1,28 @@
 export const dynamic = "force-dynamic"
+
 import { NextResponse } from "next/server"
 import connectDB from "@/lib/db"
 import Warehouse from "@/models/Warehouse"
 
 export async function GET(){
 
-  await connectDB()
+  try{
 
-  const data = await Warehouse.find().sort({createdAt:-1})
+    await connectDB()
 
-  return NextResponse.json(data)
+    const data = await Warehouse.find().sort({createdAt:-1})
+
+    return NextResponse.json({
+      success:true,
+      warehouses:data
+    })
+
+  }catch(err){
+
+    return NextResponse.json({
+      success:false,
+      warehouses:[]
+    })
+  }
+
 }
