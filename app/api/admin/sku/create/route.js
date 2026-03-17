e0e0b097084f8718218998593304016f55ad1235
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 12:51:36 +0530

    Create route.js

---
+++ app/api/admin/sku/create/route.js
@@ -0,0 +1,17 @@
+import connectDB from "@/lib/db"
+import SKU from "@/models/SKU"
+import { NextResponse } from "next/server"
+
+export async function POST(req) {
+
+  await connectDB()
+
+  const body = await req.json()
+
+  const sku = await SKU.create(body)
+
+  return NextResponse.json({
+    success: true,
+    sku
+  })
+}
