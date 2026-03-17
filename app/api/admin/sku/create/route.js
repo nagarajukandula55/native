bf1b48127d749c4254151f16440c9ecac40e2a98
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 17:48:12 +0530

    Update route.js

--- app/api/admin/sku/create/route.js
+++ app/api/admin/sku/create/route.js
@@ -1,17 +1,15 @@
 export const dynamic = "force-dynamic"
 
-import connectDB from "../../../../../lib/db"
-import Sku from "../../../../../models/Sku"
+import connectDB from "@/lib/db"
+import Sku from "@/models/Sku"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
   try {
     await connectDB()
     const body = await req.json()
-
     const sku = new Sku(body)
     await sku.save()
-
     return NextResponse.json({ success: true, sku })
   } catch (err) {
     console.error("SKU CREATE ERROR:", err)
