554fa2d74a1b0b6d2d2bc12fd8416d40689369db
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 16:07:47 +0530

    Update route.js

--- app/api/admin/warehouse/create/route.js
+++ app/api/admin/warehouse/create/route.js
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic"
 import { NextResponse } from "next/server"
 import connectDB from "@/lib/db"
 import Warehouse from "@/models/Warehouse"
@@ -23,11 +22,11 @@ export async function POST(req){
 
   }catch(err){
 
-    console.log("WAREHOUSE CREATE ERROR",err)
+    console.log(err)
 
     return NextResponse.json({
       success:false,
-      message:"Warehouse create failed"
+      message:"Warehouse create error"
     })
   }
 
