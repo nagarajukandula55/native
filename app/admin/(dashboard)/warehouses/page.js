6b12da37d4eec27e20c9d592698387bbfc5cd14a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-17 16:29:24 +0530

    Update page.js

--- app/admin/(dashboard)/warehouses/page.js
+++ app/admin/(dashboard)/warehouses/page.js
@@ -1,68 +1,35 @@
-"use client"
+async function saveWarehouse(){
 
-import { useEffect, useState } from "react"
-import Link from "next/link"
+  try{
 
-export default function WarehousesPage() {
+    const res = await fetch("/api/admin/warehouse/create",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({
+        name,
+        location
+      })
+    })
 
-  const [data, setData] = useState([])
+    const data = await res.json()
 
-  useEffect(() => {
-    fetch("/api/admin/warehouse/list")
-      .then(res => res.json())
-      .then(res => setData(res.data))
-  }, [])
+    if(data.success){
+      alert("Warehouse Added")
 
-  return (
-    <div>
+      setName("")
+      setLocation("")
 
-      <div style={{ 
-        display: "flex", 
-        justifyContent: "space-between",
-        marginBottom: 20
-      }}>
-        <h1>Warehouses</h1>
+      loadWarehouses()   // ⭐ important
+    }
+    else{
+      alert(data.message || "Save failed")
+    }
 
-        <Link href="/admin/warehouses/create">
-          <button style={btn}>+ Create</button>
-        </Link>
-      </div>
+  }catch(err){
+    alert("Server error")
+    console.log(err)
+  }
 
-      <table style={table}>
-        <thead>
-          <tr>
-            <th>Code</th>
-            <th>Name</th>
-            <th>City</th>
-            <th>State</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {data.map(w => (
-            <tr key={w._id}>
-              <td>{w.warehouseCode}</td>
-              <td>{w.warehouseName}</td>
-              <td>{w.city}</td>
-              <td>{w.state}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-    </div>
-  )
-}
-
-const btn = {
-  padding: "8px 15px",
-  background: "#111",
-  color: "#fff",
-  border: "none"
-}
-
-const table = {
-  width: "100%",
-  background: "#fff",
-  borderCollapse: "collapse"
 }
