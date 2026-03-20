a9ab78c5135adef80fa39a6b0cea050e27741166
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-20 23:23:33 +0530

    Update PaymentSettings.js

--- models/PaymentSettings.js
+++ models/PaymentSettings.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
 
-const PaymentSettingsSchema = new mongoose.Schema(
-  {
-    cod: { type: Boolean, default: true },
-    whatsapp: { type: Boolean, default: true },
-    razorpay: { type: Boolean, default: false },
+const PaymentSettingsSchema = new mongoose.Schema({
+  cod: { type: Boolean, default: true },
+  whatsapp: { type: Boolean, default: true },
+  razorpay: { type: Boolean, default: false },
+  whatsappNumber: { type: String, default: "" },
+});
 
-    whatsappNumber: { type: String, default: "" }
-  },
-  { timestamps: true }
-);
-
-const PaymentSettings =
-  mongoose.models.PaymentSettings ||
+export default mongoose.models.PaymentSettings ||
   mongoose.model("PaymentSettings", PaymentSettingsSchema);
-
-export default PaymentSettings;
