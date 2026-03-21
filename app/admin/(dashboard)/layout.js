3ee1f4eccb2ff78251650d1a2b683fa069729094
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-21 22:37:22 +0530

    Update layout.js

--- app/admin/(dashboard)/layout.js
+++ app/admin/(dashboard)/layout.js
@@ -1,9 +1,22 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import ClientSidebar from "./ClientSidebar"; // import the client component
+import ClientSidebar from "./(dashboard)/ClientSidebar";
 
 export default function AdminLayout({ children }) {
   const token = cookies().get("adminToken")?.value;
+
+  // Get current path
+  const path = typeof window === "undefined" ? "" : window.location.pathname;
+
+  // ⚠️ Allow store routes WITHOUT admin login
+  if (
+    path?.startsWith("/admin/store/login") ||
+    path?.startsWith("/admin/store/create")
+  ) {
+    return <>{children}</>;
+  }
+
+  // Protect only admin dashboard
   if (!token) redirect("/admin/login");
 
   return <ClientSidebar>{children}</ClientSidebar>;
