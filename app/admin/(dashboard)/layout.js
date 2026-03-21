ea38b53bb03d248fbae63668324dd95abdee96a5
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-22 03:33:02 +0530

    Update layout.js

--- app/admin/(dashboard)/layout.js
+++ app/admin/(dashboard)/layout.js
@@ -1,22 +1,5 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import ClientSidebar from "./ClientSidebar";
 
 export default function AdminLayout({ children }) {
-  const cookieStore = cookies();
-
-  const token = cookieStore.get("token")?.value;
-  const role = cookieStore.get("role")?.value;
-
-  /* ================= AUTH CHECK ================= */
-  if (!token) {
-    redirect("/login");
-  }
-
-  /* ================= ROLE CHECK ================= */
-  if (role !== "admin") {
-    redirect("/login");
-  }
-
   return <ClientSidebar>{children}</ClientSidebar>;
 }
