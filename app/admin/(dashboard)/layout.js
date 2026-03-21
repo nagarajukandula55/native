248075dc56262b6a107cad915c7c452a7d1739ed
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-21 22:40:44 +0530

    Update layout.js

--- app/admin/(dashboard)/layout.js
+++ app/admin/(dashboard)/layout.js
@@ -1,6 +1,6 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import ClientSidebar from "./(dashboard)/ClientSidebar";
+import ClientSidebar from "./ClientSidebar";
 
 export default function AdminLayout({ children }) {
   const token = cookies().get("adminToken")?.value;
