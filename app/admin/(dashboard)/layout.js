3cc4ba2596a1828851634a4f5482bef75f0156cc
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-24 01:22:04 +0530

    Update layout.js

--- app/admin/(dashboard)/layout.js
+++ app/admin/(dashboard)/layout.js
@@ -1,5 +1,25 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import jwt from "jsonwebtoken";
 import ClientSidebar from "./ClientSidebar";
 
 export default function AdminLayout({ children }) {
+  const token = cookies().get("token")?.value;
+
+  if (!token) {
+    redirect("/login");
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.role !== "admin") {
+      redirect("/login");
+    }
+
+  } catch (err) {
+    redirect("/login");
+  }
+
   return <ClientSidebar>{children}</ClientSidebar>;
 }
