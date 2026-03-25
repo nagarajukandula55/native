f269d4ae748bef56cd33b8851f8db066e8e7f1f5
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-25 14:57:13 +0530

    Create route.js

---
+++ app/api/auth/logout/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const response = NextResponse.json({ success: true });
+
+  response.cookies.set("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+    expires: new Date(0), // delete cookie
+  });
+
+  return response;
+}
