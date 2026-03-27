c857afa8d1c9a2b254cfe29251487b08eb58d13d
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-27 12:13:28 +0530

    Create post.js

---
+++ models/post.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  platform: { type: String, enum: ["Instagram", "Facebook", "Twitter", "LinkedIn"], required: true },
+  content: String,
+  imageUrl: String,
+  hashtags: [String],
+  templateUsed: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
