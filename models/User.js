217f49c6f4645940605fb091627351f53bfde4ef
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-27 11:32:31 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["user", "admin", "store"],
+      enum: ["user", "admin", "store","branding"],
       default: "user",
     },
 
