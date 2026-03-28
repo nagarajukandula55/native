e3c15d9fd988da90d988f747f0edc7c2a525b532
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-28 11:56:28 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,27 +2,26 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
 
     role: {
       type: String,
-      enum: ["user", "admin", "store","branding"],
+      enum: ["user", "admin", "store", "branding"],
       default: "user",
     },
 
-    warehouseId: {
-      type: String, // only for store users
-      default: null,
+    // For store users
+    warehouseId: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Warehouse", 
+      default: null 
     },
+    warehouseName: { type: String, default: null },
+    warehouseCode: { type: String, default: null },
+
+    isActive: { type: Boolean, default: true }, // optional but useful for deactivating users
   },
   { timestamps: true }
 );
