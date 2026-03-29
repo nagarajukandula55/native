54e712cb7123278771b5d98a6361f00e4643e07a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-03-29 20:01:37 +0530

    Update Inventory.js

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,40 +1,42 @@
 import mongoose from "mongoose";
 
-const InventorySchema = new mongoose.Schema({
+const InventorySchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
 
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true
+    warehouseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Warehouse",
+      required: true,
+    },
+
+    availableQty: { type: Number, default: 0, min: 0 },
+    reservedQty: { type: Number, default: 0, min: 0 },
+    shippedQty: { type: Number, default: 0, min: 0 },
+
+    totalQty: { type: Number, default: 0 },
   },
+  { timestamps: true }
+);
 
-  warehouseId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Warehouse",
-    required: true
-  },
-
-  availableQty: { type: Number, default: 0 },
-  reservedQty: { type: Number, default: 0 },
-  shippedQty: { type: Number, default: 0 },
-
-  totalQty: { type: Number, default: 0 }
-
-}, { timestamps: true });
-
+/* UNIQUE PRODUCT + WAREHOUSE */
 InventorySchema.index(
   { productId: 1, warehouseId: 1 },
   { unique: true }
 );
 
+/* AUTO TOTAL */
 InventorySchema.pre("save", function (next) {
   this.totalQty =
     (this.availableQty || 0) +
     (this.reservedQty || 0) +
     (this.shippedQty || 0);
-
   next();
 });
 
 export default mongoose.models.Inventory ||
-mongoose.model("Inventory", InventorySchema);
+  mongoose.model("Inventory", InventorySchema);
