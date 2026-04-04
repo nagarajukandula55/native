21f88351c5d516c250e19e784162136ff362ed7e
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-04 23:04:32 +0530

    Update route.js

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -1,24 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
 export async function POST() {
-  try {
-    const res = NextResponse.json({
-      success: true,
-      message: "Logged out",
-    });
+  const response = NextResponse.json({
+    success: true,
+    message: "Logged out successfully",
+  })
 
-    res.cookies.set("token", "", {
-      httpOnly: true,
-      path: "/",
-      maxAge: 0, // 🔥 delete cookie
-    });
+  response.cookies.set("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    path: "/",
+  })
 
-    return res;
-
-  } catch (err) {
-    return NextResponse.json({
-      success: false,
-      message: "Logout failed",
-    });
-  }
+  return response
 }
