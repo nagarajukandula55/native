818c4fee249a6d9fb95ce057671729d1414d02b2
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-05 20:21:54 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 
+const GSTOptionSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  hsn: { type: String, required: true },
+  gst: { type: Number, required: true },
+});
+
 const CategorySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
-    slug: { type: String, required: true, unique: true },
-    isActive: { type: Boolean, default: true },
+    name: { type: String, required: true },
+    slug: { type: String, unique: true },
+    type: { type: String, enum: ["website", "gst"], required: true },
+    gstOptions: [GSTOptionSchema], // Only for GST categories
+    active: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.models.Category ||
-  mongoose.model("Category", CategorySchema);
+export default mongoose.models.Category || mongoose.model("Category", CategorySchema);
