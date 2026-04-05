43a7648a12cc1c1e6d19c156237b32b3e664457c
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-05 14:21:50 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,33 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const schema = new mongoose.Schema(
-  {
-    name: String,
-    email: { type: String, unique: true },
-    password: String,
+const schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
 
-    role: {
-      type: String,
-      enum: ["admin", "store", "user", "branding"],
-      default: "user",
-    },
-
-    // 🔥 store = warehouse
-    warehouseId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Warehouse",
-    },
-
-    isActive: { type: Boolean, default: true },
+  role: {
+    type: String,
+    enum: ["admin", "store", "user", "branding"],
+    default: "user",
   },
-  { timestamps: true }
-);
+
+  warehouseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Warehouse",
+  },
+
+  isActive: { type: Boolean, default: true },
+}, { timestamps: true });
+
+schema.pre("save", async function(next){
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password,10);
+  next();
+});
+
+schema.methods.comparePassword = function(pw){
+  return bcrypt.compare(pw,this.password);
+};
 
 export default mongoose.models.User || mongoose.model("User", schema);
