7b68f89ca17d4089e92500c923cd955f4f5162e1
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-05 18:49:25 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -2,22 +2,9 @@ import mongoose from "mongoose";
 
 const CategorySchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    slug: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    isActive: {
-      type: Boolean,
-      default: true,
-    },
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
