3854e4905f4eaf8f02f6f32934d15483c059e0e7
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-14 22:18:57 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 
 export function middleware(req) {
   const token = req.cookies.get("token")?.value;
-  const url = req.nextUrl.pathname;
+  const path = req.nextUrl.pathname;
 
-  if (url.startsWith("/admin") || url.startsWith("/store")) {
+  if (path.startsWith("/admin")) {
     if (!token) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
@@ -13,11 +13,7 @@ export function middleware(req) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      if (url.startsWith("/admin") && decoded.role !== "admin") {
-        return NextResponse.redirect(new URL("/", req.url));
-      }
-
-      if (url.startsWith("/store") && decoded.role !== "store") {
+      if (decoded.role !== "admin") {
         return NextResponse.redirect(new URL("/", req.url));
       }
 
