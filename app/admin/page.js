b145f57006c89e6b48da7f2bb620d8cf42af707a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-14 22:25:14 +0530

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Admin Dashboard ✅</h1>
+      <p>Login & redirect working perfectly.</p>
+    </div>
+  );
+}
