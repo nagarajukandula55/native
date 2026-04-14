77e73ff8bb2a4e444452adf8ad27b3d7de673ddb
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-14 22:08:42 +0530

    Delete app/unauthorized directory

--- app/unauthorized/page.js
+++
@@ -1,9 +0,0 @@
-export default function Unauthorized() {
-  return (
-    <div style={{ textAlign: "center", marginTop: 100 }}>
-      <h1 style={{ fontSize: 28, color: "#ef4444" }}>❌ Access Denied</h1>
-      <p>You do not have permission to access this page.</p>
-      <a href="/" style={{ color: "#2563eb" }}>Go Home</a>
-    </div>
-  );
-}
