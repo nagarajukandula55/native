c67cbf2f1cc17eda046baf8cb3db55a925c10a88
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-14 22:06:29 +0530

    Delete app/api directory

--- app/api/seo/generate/route.js
+++
@@ -1,9 +0,0 @@
-export async function POST(req) {
-  const { name } = await req.json();
-
-  return Response.json({
-    title: name + " | Buy Online",
-    description: "Best price for " + name,
-    keywords: [name, "buy online", "best price"],
-  });
-}
