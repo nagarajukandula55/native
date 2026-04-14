e3940b492506a9474636170695582f911c4a238a
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-14 23:32:18 +0530

    Update route.js

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -5,11 +5,16 @@ export async function GET(req) {
   try {
     const token = req.cookies.get("token")?.value;
 
-    if (!token) return NextResponse.json({ success: false });
+    if (!token) {
+      return NextResponse.json({ success: false });
+    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    return NextResponse.json({ success: true, user: decoded });
+    return NextResponse.json({
+      success: true,
+      user: decoded,
+    });
 
   } catch {
     return NextResponse.json({ success: false });
