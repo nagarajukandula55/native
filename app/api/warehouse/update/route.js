5eac65c6a0f622232a32fe8debc7453fe5cce77d
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-22 16:40:50 +0530

    Update route.js

--- app/api/warehouse/update/route.js
+++ app/api/warehouse/update/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/dbConnect";
+import dbConnect from "@/lib/db";
 import Order from "@/models/Order";
 
 export async function POST(req) {
