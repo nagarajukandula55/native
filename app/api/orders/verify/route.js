8a7193c92ecf897c9d7bb0e89b75f091849a7074
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-22 16:15:19 +0530

    Update route.js

--- app/api/orders/verify/route.js
+++ app/api/orders/verify/route.js
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+import dbConnect from "@/lib/dbConnect";
+import Order from "@/models/Order";
 
 export async function POST(req) {
   try {
+    await dbConnect();
+
     const body = await req.json();
 
     const {
@@ -18,13 +22,31 @@ export async function POST(req) {
       .update(razorpay_order_id + "|" + razorpay_payment_id)
       .digest("hex");
 
-    if (generatedSignature === razorpay_signature) {
-      return NextResponse.json({ success: true });
+    if (generatedSignature !== razorpay_signature) {
+      return NextResponse.json({ success: false });
     }
 
-    return NextResponse.json({ success: false });
+    // 1️⃣ find order
+    const order = await Order.findOne({
+      "payment.razorpay_order_id": razorpay_order_id,
+    });
+
+    if (!order) {
+      return NextResponse.json({ success: false });
+    }
+
+    // 2️⃣ update order
+    order.status = "PAID";
+    order.payment.razorpay_payment_id = razorpay_payment_id;
+    order.payment.razorpay_signature = razorpay_signature;
+    order.payment.paidAt = new Date();
+
+    await order.save();
+
+    return NextResponse.json({ success: true });
 
   } catch (err) {
+    console.error(err);
     return NextResponse.json({ success: false });
   }
 }
