9286f421991dbf95c01885428fe398f0c418c8b2
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-22 16:23:55 +0530

    Update route.js

--- app/api/orders/verify/route.js
+++ app/api/orders/verify/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import crypto from "crypto";
-import dbConnect from "@/lib/dbConnect";
+import dbConnect from "@/lib/db";
 import Order from "@/models/Order";
 
 export async function POST(req) {
