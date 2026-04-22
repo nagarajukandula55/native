044c28c2d16738b7bed255d2b3507bbc0a8e13fa
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-22 12:03:01 +0530

    Update AuthContext.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -5,7 +5,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(undefined); // IMPORTANT
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const fetchUser = async () => {
