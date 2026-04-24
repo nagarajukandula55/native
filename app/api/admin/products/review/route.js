e8e2b6cc8e775635d13e30e1719a033f8bf698a9
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-24 09:34:36 +0530

    Create route.js

---
+++ app/api/admin/products/review/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import Product from "@/models/Product";
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const products = await Product.find({
+      status: "review",
+    }).sort({ createdAt: -1 });
+
+    return NextResponse.json({ success: true, products });
+
+  } catch (err) {
+    return NextResponse.json({ success: false, products: [] });
+  }
+}
