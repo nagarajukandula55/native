cb9cb3cf8d78c411fd6e74f9ba5a95703565caae
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-24 09:43:26 +0530

    Create route.js

---
+++ app/api/products/approve/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import Product from "@/models/Product";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const { productKey } = await req.json();
+
+    await Product.updateMany(
+      { productKey },
+      {
+        $set: {
+          status: "approved",
+          isActive: true,
+        },
+      }
+    );
+
+    return NextResponse.json({ success: true });
+
+  } catch (err) {
+    console.error("APPROVE ERROR:", err);
+
+    return NextResponse.json(
+      { success: false },
+      { status: 500 }
+    );
+  }
+}
