e7b3319e00ff2b24367a0460c6249305bcc31ce1
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-26 09:20:27 +0530

    Update page.js

--- app/admin/products/page.js
+++ app/admin/products/page.js
@@ -130,7 +130,7 @@ export default function ProductUpload() {
         ...nameWords,
         categoryname,
         subcategoryname,
-        ...ingredientWords,
+        ...ingredientNames,
         "buy online",
         "best price",
         `${cleanName} online`,
