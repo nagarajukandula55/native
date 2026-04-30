a4ab17b458807f40d9d992a6c7212a6843293069
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-30 21:24:09 +0530

    Create Counter.js

---
+++ models/Counter.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const CounterSchema = new mongoose.Schema({
+  name: { type: String, unique: true },
+  seq: { type: Number, default: 0 },
+});
+
+export default mongoose.models.Counter ||
+  mongoose.model("Counter", CounterSchema);
