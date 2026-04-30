ae3b25d692d1a69365e78063c98b8f58794aecb0
nagarajukandula55 <anenterprises9396@gmail.com>
2026-04-30 15:45:02 +0530

    Update route.js

--- app/api/products/[slug]/route.js
+++ app/api/products/[slug]/route.js
@@ -1,4 +1,6 @@
-import ProductView from "./ProductView";
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/db";
+import Product from "@/models/Product";
 
 /* ================= SEO ================= */
 export async function generateMetadata({ params }) {
