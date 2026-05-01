943f20684c2036f9334972a3e6af99bf972b5340
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-01 17:52:32 +0530

    Update page.js

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -344,17 +344,7 @@ export default function CheckoutPage() {
 
         {gstData && (
           <div className="gstBox">
-            <strong>GST Format Verified ✅</strong>
-        
-            //<div className="gstRow">
-             // <span>Company:</span>
-             // <span>{gstData.legalName || "N/A"}</span>
-           // </div>
-        
-           // <div className="gstRow">
-           //   <span>Trade Name:</span>
-            //  <span>{gstData.tradeName || "N/A"}</span>
-          //  </div>
+            <strong>GST Format Verified ✅</strong>     
         
             <div className="gstRow">
               <span>State:</span>
