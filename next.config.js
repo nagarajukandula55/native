cd1dd15a2406c615e3b4034711bb6e8765586a13
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-11 07:40:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
