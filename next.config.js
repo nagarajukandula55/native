57497ef91e75c074007a1c66e52597febdb06186
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-12 08:30:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+
+    serverExternalPackages: ["pdfkit"],
 
   webpack: (config) => {
     config.resolve.alias.canvas = false;
