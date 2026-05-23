01fc94ac36d4abbd2e48021f9564db7add4e1f20
nagarajukandula55 <anenterprises9396@gmail.com>
2026-05-23 23:23:02 +0530

    Update Pincode.ts

--- models/Pincode.ts
+++ models/Pincode.ts
@@ -2,61 +2,76 @@ import mongoose, {
   Connection,
 } from "mongoose";
 
-const PincodeSchema = new mongoose.Schema(
-  {
-    pincode: {
-      type: String,
-      required: true,
-      unique: true,
-      index: true,
+const PincodeSchema =
+  new mongoose.Schema(
+    {
+      pincode: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+      },
+
+      officeName: {
+        type: String,
+        default: "",
+      },
+
+      district: {
+        type: String,
+        default: "",
+      },
+
+      city: {
+        type: String,
+        default: "",
+      },
+
+      state: {
+        type: String,
+        default: "",
+      },
+
+      country: {
+        type: String,
+        default: "India",
+      },
+
+      deliveryStatus: {
+        type: String,
+        default: "Delivery",
+      },
+
+      isServiceable: {
+        type: Boolean,
+        default: true,
+      },
+
+      isCODAvailable: {
+        type: Boolean,
+        default: true,
+      },
+
+      shippingZone: {
+        type: String,
+        default: "A",
+      },
+
+      estimatedDays: {
+        type: Number,
+        default: 3,
+      },
+
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      },
     },
-
-    officeName: String,
-
-    district: String,
-
-    state: String,
-
-    country: {
-      type: String,
-      default: "India",
-    },
-
-    deliveryStatus: {
-      type: String,
-      default: "Delivery",
-    },
-
-    isServiceable: {
-      type: Boolean,
-      default: true,
-    },
-
-    isCODAvailable: {
-      type: Boolean,
-      default: true,
-    },
-
-    shippingZone: {
-      type: String,
-      default: "A",
-    },
-
-    estimatedDays: {
-      type: Number,
-      default: 3,
-    },
-
-    updatedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  {
-    timestamps: true,
-    collection: "pincodes",
-  }
-);
+    {
+      timestamps: true,
+      collection: "pincodes",
+    }
+  );
 
 export const getPincodeModel = (
   conn: Connection
