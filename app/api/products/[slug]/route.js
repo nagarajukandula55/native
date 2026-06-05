8f74c1cc89fe2d37148db33efceab49471e1c5d7
nagarajukandula55 <anenterprises9396@gmail.com>
2026-06-06 00:28:30 +0530

    Update route.js

--- app/api/products/[slug]/route.js
+++ app/api/products/[slug]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import connectDB from "@/lib/db";
 import Product from "@/models/Product";
 
 export const dynamic = "force-dynamic";
